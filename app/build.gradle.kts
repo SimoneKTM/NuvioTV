1c6f8854cda3d3d98a5bb3002f85901a27e7493f
SimoneKTM <kittemuort001@gmail.com>
2026-08-08 08:23:16 +0200

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -109,8 +109,8 @@ android {
         applicationId = "com.nuvio.tv"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1048
-        versionName = "1.0.3"
+        versionCode = 1049
+        versionName = "1.0.4"
 
         buildConfigField("String", "PARENTAL_GUIDE_API_URL", "\"${localProperties.getProperty("PARENTAL_GUIDE_API_URL", "")}\"")
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
