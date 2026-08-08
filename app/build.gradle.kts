f5a7fbece2cc1b6c344d82627cf5dc5a70f5cf12
SimoneKTM <kittemuort001@gmail.com>
2026-08-08 23:08:29 +0200

    chore(release) bump version to 1.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -109,8 +109,8 @@ android {
         applicationId = "com.nuvio.tv"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1053
-        versionName = "1.0.8"
+        versionCode = 1054
+        versionName = "1.0.9"
 
         buildConfigField("String", "PARENTAL_GUIDE_API_URL", "\"${localProperties.getProperty("PARENTAL_GUIDE_API_URL", "")}\"")
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
